5fb783579edef2b2361c41621d54b3783587b150
Samuel Riedel <sriedel@iis.ee.ethz.ch>
2022-05-08 14:16:46 +0200

    WIP: Tune 4x4 matmul

--- software/runtime/synchronization.c
+++ software/runtime/synchronization.c
@@ -10,12 +10,13 @@
 #include "runtime.h"
 #include "synchronization.h"
 
-uint32_t volatile barrier __attribute__((section(".l1")));
+// uint32_t volatile barrier __attribute__((section(".l1")));
+uint32_t volatile __l1_start;
 
 void mempool_barrier_init(uint32_t core_id) {
   if (core_id == 0) {
     // Initialize the barrier
-    barrier = 0;
+    __l1_start = 0;
     wake_up_all();
     mempool_wfi();
   } else {
@@ -25,8 +26,8 @@ void mempool_barrier_init(uint32_t core_id) {
 
 void mempool_barrier(uint32_t num_cores) {
   // Increment the barrier counter
-  if ((num_cores - 1) == __atomic_fetch_add(&barrier, 1, __ATOMIC_RELAXED)) {
-    __atomic_store_n(&barrier, 0, __ATOMIC_RELAXED);
+  if ((num_cores - 1) == __atomic_fetch_add(&__l1_start, 1, __ATOMIC_RELAXED)) {
+    __atomic_store_n(&__l1_start, 0, __ATOMIC_RELAXED);
     __sync_synchronize(); // Full memory barrier
     wake_up_all();
   }
